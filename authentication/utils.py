63b6c88f86753c9abc48550723b22d9c2469e49d
Driveronhire <martech@driveronhire.com>
2023-09-28 16:32:25 +0530

    Add user

--- authentication/utils.py
+++ authentication/utils.py
@@ -1,8 +1,9 @@
 import random
 
-def username_gene():
-    generate=random.randint(0,1234567)
+def username_gene(phone):
+    phone=123456789
+    generate=random.randint(0,phone)
     return generate
 
-result= username_gene()
+result= username_gene(12365478)
 print(f"Random number generate:{result}")
