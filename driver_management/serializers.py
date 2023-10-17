8d95bcbca5abc2ba75320eca29b994bc23f90e87
Driveronhire <martech@driveronhire.com>
2023-10-17 18:32:09 +0530

    changes in add driver

--- driver_management/serializers.py
+++ driver_management/serializers.py
@@ -27,7 +27,7 @@ class MyDriverSerializer(serializers.ModelSerializer):
     class Meta:
         model = AddDriver
         fields = ('first_name', 'sex', 'date_of_birth', 'driver_type', 'branch', 'zone', 't_address', 'p_address','mobile',
-                  'driving_licence','licence_no', 'licence_issued_from', 
+                  'licence_no', 'licence_issued_from', 
                   'licence_type', 'date_of_issue', 'date_of_expiry', 'present_salary', 'start_doh_date', 'aggrement_expiry_date')
 
 
