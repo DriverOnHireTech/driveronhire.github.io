7eb87b2a4907e1b1db50a4a1fd8907b56236339e
DriverOnHireTech <martech@driveronhire.com>
2023-10-18 17:45:56 +0530

    new commit

--- driver_management/views.py
+++ driver_management/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+froen rest_framework import generics
 from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
