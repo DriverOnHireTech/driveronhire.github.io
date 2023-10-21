14030aa1a0abc806fe29a5357d7c2d6009316f5f
Driveronhire <martech@driveronhire.com>
2023-10-21 18:13:28 +0530

    agent booking

--- booking/views.py
+++ booking/views.py
@@ -307,5 +307,27 @@ class Agentbookingview(APIView):
         except:
             return Response({'msg':'Booking not done', 'error':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         
+
+
+    def get(self, request):
+        try:
+
+            mobile_number= request.GET.get('mobile_number')
+            client_name= request.GET.get('client_name')
+
+            clinet_data=AgentBooking.objects.filter(mobile_number=mobile_number, client_name=client_name)
+            print("client data", clinet_data)
+            if clinet_data.exists():
+                serializer=AgentBooking(clinet_data, many=True)
+                print("Filter Data", serializer)
+                return Response({'msg':'Customer Data','data':serializer.data}, status=status.HTTP_200_OK)
+            
+            else:
+                return Response({'msg':'No Data Found'}, status=status.HTTP_204_NO_CONTENT)
+            
+        except AgentBooking.DoesNotExist:
+            return Response({'msg':'No Data Found', 'error':serializer.errors}, status=status.HTTP_204_NO_CONTENT)
+            
+        
             
         
