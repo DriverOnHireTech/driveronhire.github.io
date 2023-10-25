71f6bc64ee9848a9285ae37a7fa8da2e0ae394ee
Driveronhire <martech@driveronhire.com>
2023-10-25 21:43:53 +0530

    add email

--- base_site/settings.py
+++ base_site/settings.py
@@ -221,8 +221,8 @@ TWILIO_PHONE_NUMBER = '+12107141446'
 #Email Send setup
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'martech@driveronhire.com'
-EMAIL_HOST_PASSWORD = 'User@doh2527'
+EMAIL_HOST_USER = 'farooqaziz1993@gmail.com'
+EMAIL_HOST_PASSWORD = 'zdxlkvtlwnihemsf'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
