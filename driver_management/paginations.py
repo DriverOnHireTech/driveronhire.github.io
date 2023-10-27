6112edc731f2b1a99fc954ce14eddafa6e0178d1
DriverOnHireTech <martech@driveronhire.com>
2023-10-27 19:56:16 +0530

    pagination added

--- driver_management/paginations.py
+++ driver_management/paginations.py
@@ -1,6 +1,6 @@
-# from rest_framework import pagination
+from rest_framework import pagination
 
-# class cutomepegination(pagination.PageNumberPagination):
-#     page_size= 1
-#     page_size_query_param='count'
-#     max_page_size= 10
+class cutomepegination(pagination.PageNumberPagination):
+    page_size= 10
+    page_size_query_param='count'
+    max_page_size= 10
