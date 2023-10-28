8afe5f3317e0e35d1693a6fd3b2a9ecaef079ac5
Driveronhire <martech@driveronhire.com>
2023-10-28 13:08:36 +0530

    added

--- driver_management/admin.py
+++ driver_management/admin.py
@@ -3,7 +3,7 @@ from .models import *
 
 admin.site.register(BasicDetail)
 admin.site.register(ViewDriver)
-admin.site.register(AddDriver)
+# admin.site.register(AddDriver)
 # admin.site.register(Bookingstatus)
 admin.site.register(ReferDriver)
 admin.site.register(Driverleave)
@@ -15,7 +15,7 @@ admin.site.register(Driverlocation)
 class Adddriveradmin(admin.AdminSite):
     fields=['first_name','driver_type', 'mobile']
     list_filter=('mobile')
-    
+
 admin.site.register(AddDriver, Adddriveradmin)
 
 
