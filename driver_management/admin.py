e2593efe4aaa35b8a33060057b55fbd041c0d11e
Driveronhire <martech@driveronhire.com>
2023-10-28 13:04:02 +0530

    added field in driver

--- driver_management/admin.py
+++ driver_management/admin.py
@@ -10,6 +10,14 @@ admin.site.register(Driverleave)
 admin.site.register(DriverBalance)
 admin.site.register(Driverlocation)
 
+
+
+class Adddriveradmin(admin.AdminSite):
+    fields=['first_name','driver_type', 'mobile']
+
+admin.site.register(AddDriver, Adddriveradmin)
+
+
 class Driverappstatusadmin(admin.ModelAdmin):
     fields=['drivername', 'package', 'paymentamount', 'is_paid', 'status']
 admin.site.register(Driverappstatus, Driverappstatusadmin)
