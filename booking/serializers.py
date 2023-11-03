3a06f99e33f118060034b6210e278d26c728e316
DriverOnHireTech <martech@driveronhire.com>
2023-11-03 13:33:01 +0530

    latest commit

--- booking/serializers.py
+++ booking/serializers.py
@@ -27,8 +27,8 @@ class PlacebookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlaceBooking
         
-        fields= ('id','trip_type', 'from_date',
-                  'to_date', 'car_type', 'gear_type', 
+        fields= ('id','trip_type', 
+                  'booking_date', 'car_type', 'gear_type', 
                   'pickup_location', 'drop_location', 'booking_time', 'currant_location', 'status','packege', 'mobile')
 
     # def get_drivers(self, obj):
