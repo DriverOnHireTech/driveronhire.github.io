706ea49f77387f5ba70f2c5b88506f9d78d33441
Driveronhire <martech@driveronhire.com>
2023-11-13 12:06:40 +0530

    add enquiry

--- enquiry/urls.py
+++ enquiry/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns=[
     path('send_email/', views.senddriverenquiry.as_view(), name='send_email'),
     path('single_enq/<int:id>/', views.Getsingle_enq.as_view(), name='single_enq'), # For Reterive single enquiry
+    # path()
 ]
