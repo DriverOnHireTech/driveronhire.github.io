94319d5bb095ad85160b2a45025f41ac9c6bae7b
DriverOnHireTech <martech@driveronhire.com>
2023-12-30 12:21:29 +0530

    changes in start deuty

--- booking/serializers.py
+++ booking/serializers.py
@@ -30,7 +30,7 @@ class PlacebookingSerializer(serializers.ModelSerializer):
         
         fields= ('id','trip_type', 'booking_date','no_of_days', 
                    'car_type', 'gear_type', 
-                  'pickup_location', 'client_booking_time', 'drop_location', 'booking_time', 'currant_location', 'status','packege',  'user_address', 'deuty_started', 'cancelbooking_reason', 'cancelbooking_message', 'pickup_zone', 'drop_zone')
+                  'pickup_location', 'client_booking_time', 'drop_location', 'booking_time', 'deuty_started','currant_location', 'status','packege',  'user_address','cancelbooking_reason', 'cancelbooking_message', 'pickup_zone', 'drop_zone')
    
    
 class NotifyDriverSerializer(serializers.ModelSerializer):
